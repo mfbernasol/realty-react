fa5590283fe0f858000c56334276d4e6b26b4312
mfbernasol <mfbernasol@gmail.com>
2022-02-11 17:36:24 -0600

    data fetching

--- pages/index.js
+++ pages/index.js
@@ -34,7 +34,8 @@ const Banner = ({
   </Flex>
 );
 
-export default function Home() {
+export default function Home({ propertiesForSale,propertiesForRent }) {
+  console.log(propertiesForSale,propertiesForRent);
   return (
     <Box>
       <h1>Hello World</h1>
@@ -61,4 +62,16 @@ export default function Home() {
       />
     </Box>
   );
+}
+
+export async function getStaticProps() {
+  const propertyForSale = await fetchApi(`${baseUrl}/properties/list?locationExternalIDs=5002&purpose=for-sale&hitsPerPage=6`);
+  const propertyForRent = await fetchApi(`${baseUrl}/properties/list?locationExternalIDs=5002&purpose=for-rent&hitsPerPage=6`);
+
+  return {
+    props: {
+      propertiesForSale: propertyForSale?.hits,
+      propertiesForRent: propertyForRent?.hits,
+    },
+  };
 }
