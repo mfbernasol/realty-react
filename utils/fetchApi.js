2885d0067c75d1415233994ed5cdd2fec046f029
mfbernasol <mfbernasol@gmail.com>
2022-02-11 16:20:35 -0600

    working on fetching data

---
+++ utils/fetchApi.js
@@ -0,0 +1,16 @@
+import axios from 'axios';
+
+export const baseUrl = 'bayut.p.rapidapi.com';
+
+
+
+export const fetchApi = async (url) => {
+  const {data} = await axios.get(url, {
+    headers: {
+      'x-rapidapi-host': 'bayut.p.rapidapi.com',
+      'x-rapidapi-key': 'a6c510a36amshea5b83de6760ff5p1752d1jsn196f247b280c',
+    },
+  });
+
+  return data;
+};
