131770d211daf813e60a136d01a198de8f880a7a
mfbernasol <mfbernasol@gmail.com>
2022-02-12 22:12:18 -0600

    fixing component

--- pages/property/[id].js
+++ pages/property/[id].js
@@ -3,7 +3,7 @@ import { FaBed, FaBath } from 'react-icons/fa';
 import { BsGridFill } from 'react-icons/bs';
 import { GoVerified } from 'react-icons/go';
 import millify from 'millify';
-import ImageScrollbar from '../../components/ImageScrollbar';
+import ImageScrollbar from '../../components/ImageScrollBar';
 import { baseUrl, fetchApi } from '../../utils/fetchApi';
 
 const PropertyDetails = ({ propertyDetails: { price, rentFrequency, rooms, title, baths, area, agency, isVerified, description, type, purpose, furnishingStatus, amenities, photos } }) => (
